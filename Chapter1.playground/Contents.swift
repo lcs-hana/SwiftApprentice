959cc3dcba7570745d5e8176ddc2446443c323a2
Hana Kono <hkono@lcs.on.ca>
2020-10-22 22:44:48 +0900

    Example of Math function

--- Chapter1.playground/Contents.swift
+++ Chapter1.playground/Contents.swift
@@ -30,3 +30,17 @@ print("Hello my name is Hana")
 350 / 5 + 2
 350 / (5 + 2)
 
+//Math function
+sin(45 * Double.pi / 180)
+
+cos(135 * Double.pi / 180)
+
+(2.0).squareRoot()
+
+max(5, 10)
+
+min(-5, -10)
+
+max((2.0).squareRoot(), Double.pi/2)
+
+
