521b7d76894706acf497790e01ebc66bd1b1e7e2
Hana Kono <hkono@lcs.on.ca>
2020-10-22 09:11:56 +0900

    Example of using remainder operator

--- Chapter1.playground/Contents.swift
+++ Chapter1.playground/Contents.swift
@@ -7,3 +7,10 @@ var str = "Hello, playground"
 10 - 2
 2 * 4
 24 / 3
+
+
+// Remainder operator
+// This gives you the value left over after dividing 28 by 10
+// e.g.: 10 goes into 28 twice, and 8 remains
+28 % 10
+28 / 10
