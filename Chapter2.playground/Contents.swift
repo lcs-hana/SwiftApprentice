8a3ebc39a93c9f87e9da8f6efe30d5873cdefe91
Hana Kono <hkono@lcs.on.ca>
2020-10-27 08:59:12 +0900

    simple example of type conversion.

--- Chapter2.playground/Contents.swift
+++ Chapter2.playground/Contents.swift
@@ -1,3 +1,13 @@
 import Cocoa
 
 var str = "Hello, playground"
+
+// Type Conversion
+var integer: Int = 100
+var decimal: Double = 12.5
+integer = Int(decimal)
+// intenger = decimal // this code causes an erro in swift
+
+
+
+
