aa6a6aeb633bb70dedf904738410de98768b585f
Hana Kono <hkono@lcs.on.ca>
2020-10-27 00:18:38 +0900

    Complete chapter 1

--- Chapter1.playground/Contents.swift
+++ Chapter1.playground/Contents.swift
@@ -3,7 +3,7 @@ import Cocoa
 var str = "Hello, playground"
 
 
-// Basic operators
+/* // Basic operators
 2 + 6
 10 - 2
 2 * 4
@@ -54,6 +54,85 @@ let pi : Double = 3.14159
 var variableNumber: Int = 42
 variableNumber = 0
 variableNumber = 1_000_000
+
+// Increment and decrement
+var counter: Int = 0
+
+counter += 1
+// counter = 1
+
+counter -= 1
+// counter = 0
+
+var counter: Int = 0
+counter = counter + 1
+counter = counter - 1
+
+counter = 10
+
+counter *= 3 // same as counetr = counter * 3
+// counter = 30
+
+counter /= 2 // same as counter = counter / 2
+// counter = 15 */
+
+// Challenge 1
+var myAge: Int = 17
+var dogs: Int = 1
+dogs += 1
+
+// Challenge 2
+var age: Int = 16
+print(age)
+age = 30
+print(age)
+// I used "var"
+
+// Challenge 3
+let x: Int = 46
+let y: Int = 10
+//1
+let answer1 : Int = (x * 100) + y  // answer 1 should be 4610
+//2
+let answer2 : Int = (x * 100) + (y * 100) // answer 2 should be 5600
+//3
+let answer3 : Int = (x * 100) + (y / 100)  // answer 3 should be 4600
+
+// Challenge 4
+8 - 4 * 2 + 6 / 3 * 4
+(8 - (4 * 2)) + ((6 / 3) * 4)
+
+// Challenge 5
+let rating1: Double = 4.3
+let rating2: Double = 2.3
+let rating3: Double = 2.5
+let averageRating: Double = (rating1 + rating2 + rating3) / 3
+
+// Challenge 6
+let vortage: Double = 14
+let current: Double = 42
+let power: Double = vortage * current
+
+// Challenge 7
+let resistance: Double = power / (current * current)
+
+// Challenge 8
+let randomNumber = arc4random()
+let diceRoll = (randomNumber % 6) + 1
+
+// Challenge 9
+let a: Double = 2
+let b: Double = 3
+let c: Double = 5
+let root1: Double = (-b + (b * b - 4 * a * c).squareRoot()) / (2 * a)
+let root2: Double = (-b - (b * b - 4 * a * c).squareRoot()) / (2 * a)
+
+
+
+
+
+
+
 
 
 
