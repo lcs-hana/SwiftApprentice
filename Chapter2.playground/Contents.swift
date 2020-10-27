4fba81ca7ab4424b2f83387c565a92e05c7ee832
Hana Kono <hkono@lcs.on.ca>
2020-10-27 18:59:31 +0900

    Compete mini exercise.

--- Chapter2.playground/Contents.swift
+++ Chapter2.playground/Contents.swift
@@ -2,7 +2,7 @@ import Cocoa
 
 var str = "Hello, playground"
 
-// Type Conversion
+/*// Type Conversion
 var integer: Int = 100
 var decimal: Double = 12.5
 integer = Int(decimal)
@@ -13,6 +13,29 @@ let hourlyRate: Double = 19.5
 let hoursWorked: Int = 10
 let totalCost: Double = hourlyRate * Double(hoursWorked)
 // let totalCost: Double = hourlyRate * hoursWorked // this causes an error since you can't mix types directly
+
+// Type inference
+let typeInferredInt = 42
+// holding down "option" key and click variable or caonsatnt's name.
+// In this case type of code is "Int"
+
+let typeInferredDouble = 3.14159
+let wantAdouble = 3
+let actuallyDouble = Double(3)
+let actuallyDouble: Double = 3
+let actuallyDouble = 3 as Double
+let wantDouble2 = 3.0 */
+
+// Mini - exercises
+// Exercise 1
+let age1 = 42
+let age2 = 21
+
+// Exercise 2
+let avg1 = (age1 + age2) / 2
+
+// Exercise 3
+// I don't know what should I do here.
 
 
 
