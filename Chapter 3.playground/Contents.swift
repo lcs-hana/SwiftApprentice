79092391720d9eeeaf1bf36eae965b162353356b
Hana Kono <hkono@lcs.on.ca>
2020-11-03 21:19:00 +0900

    Some examples of compassion operators and boolean operators.

---
+++ Chapter 3.playground/Contents.swift
@@ -0,0 +1,24 @@
+import Cocoa
+
+var str = "Hello, playground"
+
+//Comparsion operators
+let yes: Bool = true
+let no: Bool = false
+//let yes = true
+//let no = false
+
+//Boolean operators
+let doesOneEqualTwo = (1 == 2)
+let doesOneNoteEqualTwo = (1 != 2)
+let alsoTrue = !(1 == 2)
+let isOneGreaterThanTwo = (1 > 2)
+let isOneLessThanTwo = (1 < 2)
+let and = true && true
+let or = true || false
+let andTrue = 1 < 2 && 4 > 3
+let andFalse = 1 < 2 && 3 > 4
+let orTrue = 1 < 2 || 3 > 4
+let orFalse = 1 == 2 || 3 == 4
+let andOr = (1 < 2 && 3 > 4) || 1 < 4
+
