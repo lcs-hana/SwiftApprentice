0b9680b1ba1413c63d2e8dd6435b3d4dc5808b00
Hana Kono <hkono@lcs.on.ca>
2020-11-03 23:26:00 +0900

    Some example of string equality.

--- Chapter 3.playground/Contents.swift
+++ Chapter 3.playground/Contents.swift
@@ -15,10 +15,16 @@ let alsoTrue = !(1 == 2)
 let isOneGreaterThanTwo = (1 > 2)
 let isOneLessThanTwo = (1 < 2)
 let and = true && true
+//and
 let or = true || false
+//or
 let andTrue = 1 < 2 && 4 > 3
 let andFalse = 1 < 2 && 3 > 4
 let orTrue = 1 < 2 || 3 > 4
 let orFalse = 1 == 2 || 3 == 4
 let andOr = (1 < 2 && 3 > 4) || 1 < 4
 
+//String equality
+let guess = "dog"
+let dogEqualsCat = guess == "cat"
+let prder = "cat" < "dog"
