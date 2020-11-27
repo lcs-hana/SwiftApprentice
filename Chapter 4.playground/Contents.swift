47c9e045b89b83f4c3ab26d16bcd9a545f0c7074
Hana Kono <hkono@lcs.on.ca>
2020-11-27 09:05:16 -0500

    Accomplish mini-exercise

--- Chapter 4.playground/Contents.swift
+++ Chapter 4.playground/Contents.swift
@@ -186,3 +186,44 @@ case let (x, y, _) where y == x * x:
 default:
     break
 }
+
+// Mini-exercises
+// Exercise 1
+let age: Int = 63
+
+switch age {
+case 0...2:
+    print("Infant")
+case 3...12:
+    print("Child")
+case 13...19:
+    print("Teenager")
+case 20...39:
+    print("Adult")
+case 40...60:
+    print("Middle aged")
+case let x where x >= 61:
+    print("Elderly")
+default:
+    fatalError()
+}
+
+// Exercise 2
+let person: (name: String, age2: Int) = ("Steve", 31)
+
+switch person {
+case (let name, 0...2):
+    print("\(name) is an infant.")
+case (let name, 3...12):
+    print("\(name) is a child.")
+case (let name, 13...19):
+    print("\(name) is a teenager.")
+case (let name, 20...39):
+    print("\(name) is an adult.")
+case (let name, 40...60):
+    print("\(name) is middle aged.")
+case let (name, age2) where age2 >= 61:
+    print("\(name) is elderly.")
+default:
+    fatalError()
+}
