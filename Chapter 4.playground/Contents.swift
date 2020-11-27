377b37ca7e3641aeb31715c1b78e50486b72a79a
Hana Kono <hkono@lcs.on.ca>
2020-11-27 09:04:31 -0500

    Example of partial matching

--- Chapter 4.playground/Contents.swift
+++ Chapter 4.playground/Contents.swift
@@ -149,3 +149,40 @@ case _ where number % 2 == 0:
 default:
     print("Odd")
 }
+
+// Partial matching
+let coordinates = (x: 3, y: 2, z: 5)
+switch coordinates {
+case (0, 0, 0): // 1
+    print("Origin")
+case (_, 0, 0): // 2
+    print("On the x-axis.")
+case (0, _, 0): // 3
+    print("On the y-axis.")
+case (0, 0, _): // 4
+    print("On the z-axis.")
+default: // 5
+    print("Somewhere in space")
+}
+
+switch coordinates {
+case (0, 0, 0):
+    print("Origin")
+case (let x, 0, 0):
+    print("On the x-axis at x = \(x)")
+case (0, let y, 0):
+    print("On the y-axis at y = \(y).")
+case (0, 0, let z):
+    print("On the z-axis at z = \(z)")
+case let (x, y, z):
+    print("Somewhere in space at x = \(x), y = \(y), z = \(z)")
+}
+
+switch coordinates {
+case let (x, y, _) where y == x:
+    print("Along the y = x line.")
+case let (x, y, _) where y == x * x:
+    print("Along the y = x^2 line.")
+default:
+    break
+}
